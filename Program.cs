86615c582595142ddeff49b6280a372b7c95cb3b
MarcusVHart <haykirengeral@gmail.com>
2025-03-29 21:05:57 -0300

    criação de sistema que troca R por L

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Digite uma frase e veja como o Cebolinha falaria:");
+string Frase = Console.ReadLine() ?? "";
+string cebolinha = Frase.Replace("r", "l").Replace("R", "L");
+
+Console.WriteLine(cebolinha);
+
